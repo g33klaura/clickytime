82a2deb541aa9c5d4e28bbe438b34221a0150e95
Laura Boetcher <g33klaura@gmail.com>
2017-12-27 22:16:46 -0500

    Add game square columns structure

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './styles/index.css';
+import { Button, Card, Row, Col } from 'react-materialize';
+import '../styles/Game.css';
 
-const Game = props =>
-  <p>Mathematical!</p>;
+const Game = props => (
+  <main className="squares">
+    <Row>
+      <Col l={3} m={4} s={6}><p>1</p></Col>
+      <Col l={3} m={4} s={6}><p>2</p></Col>
+      <Col l={3} m={4} s={6}><p>3</p></Col>
+      <Col l={3} m={4} s={6}><p>4</p></Col>
+      <Col l={3} m={4} s={6}><p>5</p></Col>
+      <Col l={3} m={4} s={6}><p>6</p></Col>
+      <Col l={3} m={4} s={6}><p>7</p></Col>
+      <Col l={3} m={4} s={6}><p>8</p></Col>
+      <Col l={3} m={4} s={6}><p>9</p></Col>
+      <Col l={3} m={4} s={6}><p>10</p></Col>
+      <Col l={3} m={4} s={6}><p>11</p></Col>
+      <Col l={3} m={4} s={6}><p>12</p></Col>
+    </Row>
+  </main>
+);
 
 export default Game;
