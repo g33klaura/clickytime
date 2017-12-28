e3680b8f7e8ddfbd8431e7aa1096dd1926d5bc56
Laura Boetcher <g33klaura@gmail.com>
2017-12-28 00:03:18 -0500

    Add shadows to game squares

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { Button, Card, Row, Col } from 'react-materialize';
+import { Button, Card, Row, Col, CardTitle } from 'react-materialize';
 import '../styles/Game.css';
 // import Squares from "./Squares.js";
 
@@ -27,30 +27,54 @@ const Game = props => (
   <main className="squares">
     <Row>
       <Col l={3} m={4} s={6}>
-      <img src={squares["abracadaniel.png"]} alt="abracadaniel" />
+        {/*<Card className="small" header={<CardTitle image={squares["abracadaniel.png"]}></CardTitle>}>
+        </Card>*/}
+        <img src={squares["abracadaniel.png"]} alt="abracadaniel" className="z-depth-4" />
       </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["billycanyon.png"]} alt="billy_and_canyon" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["billycanyon.png"]} alt="billy_and_canyon" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["goliad.png"]} alt="goliad" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["goliad.png"]} alt="goliad" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["jake.png"]} alt="jake" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["jake.png"]} alt="jake" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["lspmissing.png"]} alt="lsp_missing_lumps" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["lspmissing.png"]} alt="lsp_missing_lumps" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["memow.png"]} alt="memow" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["memow.png"]} alt="memow" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["newlemongrab.png"]} alt="lemongrab_all_better" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["newlemongrab.png"]} alt="lemongrab_all_better" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["princesscookie.png"]} alt="princess_cookie" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["princesscookie.png"]} alt="princess_cookie" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["prismo.png"]} alt="prismo" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["prismo.png"]} alt="prismo" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["shelbylilbro.png"]} alt="shelby_and_little_brother" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["shelbylilbro.png"]} alt="shelby_and_little_brother" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["sweetp.png"]} alt="sweet_p" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["sweetp.png"]} alt="sweet_p" className="z-depth-4" />
+      </Col>
 
-      <Col l={3} m={4} s={6}><img src={squares["treetrunks.png"]} alt="treetrunks" /></Col>
+      <Col l={3} m={4} s={6}>
+        <img src={squares["treetrunks.png"]} alt="treetrunks" className="z-depth-4" />
+      </Col>
     </Row>
   </main>
 );
