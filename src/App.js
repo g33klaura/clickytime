4e8deb70912721fdda3ae1768b734f99f0dd60e4
Laura Boetcher <g33klaura@gmail.com>
2017-12-27 22:17:14 -0500

    Update semantic div names & intro message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, Row, Col } from 'react-materialize';
+import { Button, Card, Row, Col, Container } from 'react-materialize';
 // import logo from './logo.svg';
 import './styles/App.css';
 import Game from './components/Game';
@@ -8,22 +8,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Container">
+        {/*<div className="Container">*/}
+        <Container>
 
           <header className="App-header">
             {/*<img src={logo} className="App-logo" alt="logo" />*/}
             <h1 className="App-title">ClickyTime</h1>
           </header>
           <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
+            How good is your memory? Click on each square only once!
           </p>
           <Row>
-            <Col s={12}>
+            <Col s={12} className={"center-align"}>
               <Game />
             </Col>
           </Row>
-          
-        </div>
+
+        </Container>
       </div>
     );
   }
