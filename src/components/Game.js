8de5fb617b08aaf411ddca9e2f60ef138db833f9
Laura Boetcher <g33klaura@gmail.com>
2017-12-27 22:45:13 -0500

    First image rendered with import (ah HA)

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Card, Row, Col } from 'react-materialize';
 import '../styles/Game.css';
+// import Squares from "./Squares.js";
+
+// Images need imports too???
+import abracadaniel from '../img/abracadaniel.png';
+
+// Need to make each square image a component? then import Squares.js? 
 
 const Game = props => (
   <main className="squares">
     <Row>
-      <Col l={3} m={4} s={6}><p>1</p></Col>
-      <Col l={3} m={4} s={6}><p>2</p></Col>
+      <Col l={3} m={4} s={6}><img src={"http://lorempixel.com/output/animals-q-c-200-200-10.jpg"} alt="abracadaniel" /></Col>
+      <Col l={3} m={4} s={6}><img src={abracadaniel} /></Col>
       <Col l={3} m={4} s={6}><p>3</p></Col>
       <Col l={3} m={4} s={6}><p>4</p></Col>
       <Col l={3} m={4} s={6}><p>5</p></Col>
---
+++ src/components/Squares.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// const Squares = props => 
+//   (const abracadaniel = URL('../img/abracadaniel.png'););
+
+
+// export default Squares;
