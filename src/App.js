e23b87b3a2a079450a92cdb23f9e47dde47269dc
Laura Boetcher <g33klaura@gmail.com>
2017-12-27 21:38:27 -0500

    Add initial Game component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Button, Card, Row, Col } from 'react-materialize';
 // import logo from './logo.svg';
 import './styles/App.css';
+import Game from './components/Game';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">ClickyTime</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Container">
+
+          <header className="App-header">
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <h1 className="App-title">ClickyTime</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Row>
+            <Col s={12}>
+              <Game />
+            </Col>
+          </Row>
+          
+        </div>
       </div>
     );
   }
---
+++ src/components/Game.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './styles/index.css';
+
+const Game = props =>
+  <p>Mathematical!</p>;
+
+export default Game;
