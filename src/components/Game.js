98f10d25879bdc7a9cb73cacfaa4eac757c27fb1
Laura Boetcher <g33klaura@gmail.com>
2017-12-27 23:35:43 -0500

    Add function to import all images from folder

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,29 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { Button, Card, Row, Col } from 'react-materialize';
 import '../styles/Game.css';
 // import Squares from "./Squares.js";
 
 // Images need imports too???
-import abracadaniel from '../img/abracadaniel.png';
+// import abracadaniel from '../img/abracadaniel.png';
 
-// Need to make each square image a component? then import Squares.js? 
+// **************************************
+// Map over each image in 'img' rather than importing each one
+// Thank you this guy: https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+function importAll(squareImg) {
+  let squares = {};
+  squareImg.keys().map((item, index) => { 
+    squares[item.replace('./', '')] = squareImg(item); 
+  });
+  return squares;
+}
+
+const squares = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
+
+// <img src={images['doggy.png']} />
+// **************************************
 
 const Game = props => (
   <main className="squares">
     <Row>
-      <Col l={3} m={4} s={6}><img src={"http://lorempixel.com/output/animals-q-c-200-200-10.jpg"} alt="abracadaniel" /></Col>
-      <Col l={3} m={4} s={6}><img src={abracadaniel} /></Col>
-      <Col l={3} m={4} s={6}><p>3</p></Col>
-      <Col l={3} m={4} s={6}><p>4</p></Col>
-      <Col l={3} m={4} s={6}><p>5</p></Col>
-      <Col l={3} m={4} s={6}><p>6</p></Col>
-      <Col l={3} m={4} s={6}><p>7</p></Col>
-      <Col l={3} m={4} s={6}><p>8</p></Col>
-      <Col l={3} m={4} s={6}><p>9</p></Col>
-      <Col l={3} m={4} s={6}><p>10</p></Col>
-      <Col l={3} m={4} s={6}><p>11</p></Col>
-      <Col l={3} m={4} s={6}><p>12</p></Col>
+      <Col l={3} m={4} s={6}>
+      <img src={squares["abracadaniel.png"]} alt="abracadaniel" />
+      </Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["billycanyon.png"]} alt="billy_and_canyon" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["goliad.png"]} alt="goliad" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["jake.png"]} alt="jake" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["lspmissing.png"]} alt="lsp_missing_lumps" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["memow.png"]} alt="memow" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["newlemongrab.png"]} alt="lemongrab_all_better" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["princesscookie.png"]} alt="princess_cookie" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["prismo.png"]} alt="prismo" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["shelbylilbro.png"]} alt="shelby_and_little_brother" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["sweetp.png"]} alt="sweet_p" /></Col>
+
+      <Col l={3} m={4} s={6}><img src={squares["treetrunks.png"]} alt="treetrunks" /></Col>
     </Row>
   </main>
 );
