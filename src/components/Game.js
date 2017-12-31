ed1c1934173db6cb841b399a7a12248ee2f90923
Laura Boetcher <g33klaura@gmail.com>
2017-12-31 18:41:37 -0500

    Trying all manner of combos to target clicked image id...

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,7 +8,7 @@ import friends from "../friends.json";
 // Component class for rendering all game squares from friends.json
 class Game extends React.Component {
 
-  // Use "building block" component from Squares to render each image in the materialize divs
+
 
   // Activity 16.... how does this work now?....
   // state = {
@@ -23,25 +23,33 @@ class Game extends React.Component {
       click: false,
       color: "testing this connects" 
     };
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
     console.log(this.props);
   }
 
   handleClick = () => {
-    // this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.setState({
       click: true
     });
     console.log("Image clicked");
-    // console.log(this.props.id);
-    // ^^Don't think it's grabbing from page, think it's trying to grab from friendS.json
-    // Breaks if remove the 's'
 
-    // Undefined with this.props.id.... probably b/c that comes up undefined in browser
-    console.log(this.id);
-    // console.log(this.state.friend.id);
+    // console.log(this.friends.id);  //error "_this.friends is undefined"
+
+    console.log(friends.id);  //undefined
+      // ^^Don't think it's grabbing from page, think it's trying to grab from friendS.json
+      // Breaks if remove the 's'
+
+    // console.log(this.friend.id);  //error "_this.friend is undefined"
+
+    console.log(this.props.id);  //undefined
+
+    console.log(this.id);  //undefined
+
+    // console.log(this.state.friend.id);  //error "_this.state.friend is undefined"
   }
 
+  // Use "building block" component from Squares to render each image in the materialize divs
   render() {
     return (
       <main className="game-squares">
