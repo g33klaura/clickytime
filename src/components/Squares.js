897a3742034234576dfd131016af309099071bb8
Laura Boetcher <g33klaura@gmail.com>
2017-12-31 17:56:35 -0500

    Key & alt still undefined

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -13,10 +13,12 @@ const Squares = props => {
   // yet ARE defined in Game.js in <Squares ... />
   return (
     <Col l={3} m={4} s={6}>
-      <img src={ props.image } alt={ props.name } id={ props.id } key={ props.id } className="z-depth-4" data-click={ props.click } onClick={ props.onClick } />
+      <img src={ props.image } alt={ props.name } id={ props.id } key={ props.id } className="z-depth-4" data-click={ props.click } onClick={ () => props.onClick(props.id) } />
     </Col>
   )
 
 };
 
 export default Squares;
+
+// onClick={ props.onClick }
