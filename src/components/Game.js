b91ff454c020301ae582fd5dc467efeb585f3ede
Laura Boetcher <g33klaura@gmail.com>
2017-12-30 21:00:59 -0500

    Trying to render image from JSON

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, Card, Row, Col, CardTitle } from 'react-materialize';
+import { Row } from 'react-materialize';
 import '../styles/Game.css';
-import Squares from "./Squares.js";
+import Squares from "./Squares";
+import friends from "../friends.json";
 
 // Images need imports too???
 // import abracadaniel from '../img/abracadaniel.png';
@@ -9,89 +10,40 @@ import Squares from "./Squares.js";
 // ****************************************************************************
 // Map over each image in 'img' rather than importing each one
 // Thank you this guy: https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
-function importAll(squareImg) {
-  let squares = {};
-  squareImg.keys().map((item, index) => { 
-    squares[item.replace('./', '')] = squareImg(item); 
-  });
-  return squares;
-}
+// function importAll(squareImg) {
+//   let squares = {};
+//   squareImg.keys().map((item, index) => { 
+//     squares[item.replace('./', '')] = squareImg(item); 
+//   });
+//   return squares;
+// }
 
-const squares = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
+// const squares = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
 
 // <img src={images['doggy.png']} />
 // ****************************************************************************
 
-// Needs to be Component Class, pulling from Squares Component****
-// Think reusable blocks of code...
-{/* 
-const Game = props => (
-  <main className="squares">
-    <Row>
-      <Col l={3} m={4} s={6}>
-        
-        <img src={squares["abracadaniel.png"]} alt="abracadaniel" className="z-depth-4" />
-      </Col>
+// Moved initally hard-coded images to scratch sheet so i stop getting a warning...
 
-      <Col l={3} m={4} s={6}>
-        <img src={squares["billycanyon.png"]} alt="billy_and_canyon" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["goliad.png"]} alt="goliad" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["jake.png"]} alt="jake" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["lspmissing.png"]} alt="lsp_missing_lumps" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["memow.png"]} alt="memow" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["newlemongrab.png"]} alt="lemongrab_all_better" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["princesscookie.png"]} alt="princess_cookie" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["prismo.png"]} alt="prismo" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["shelbylilbro.png"]} alt="shelby_and_little_brother" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["sweetp.png"]} alt="sweet_p" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["treetrunks.png"]} alt="treetrunks" className="z-depth-4" />
-      </Col>
-    </Row>
-  </main>
-);
-*/}
-
-export class Game extends React.Component {
+class Game extends React.Component {
 
   // Use "building block" from Squares to render each image in the materialize divs?
 
   // Need array of squares
-
+  
+  
+  // test () => console.log({ friends[0].image });
+   
   render() {
     return (
       <main className="game-squares">
         <Row>
           {/* Each of 12 squares rendered within this row div */}
+          <Squares 
+            image={ friends[0].image }
+            alt={ friends[0].name }
+            key={ friends[0].id }
+          />
         </Row>
       </main>
     )
@@ -99,4 +51,4 @@ export class Game extends React.Component {
 
 }
 
-// export default Game;
+export default Game;
--- src/components/Squares.js
+++ src/components/Squares.js
@@ -1,66 +1,18 @@
 import React from 'react';
-import { Row, Col } from 'react-materialize';
+import { Col } from 'react-materialize';
 
 // const Squares = props => 
 //   (const abracadaniel = URL('../img/abracadaniel.png'););
 
 
 // Template for each repeating square?
-export const Squares = props => (
+const Squares = props => (
 
     // Will render in row div in Game Component?
     <Col l={3} m={4} s={6}>
-      <img src={} alt={} key={} className="z-depth-4" />
+      <img src={ props.image } alt={ props.name } key={ props.id } className="z-depth-4" />
     </Col>
 
-      {/*<Col l={3} m={4} s={6}>
-        <img src={squares["abracadaniel.png"]} alt="abracadaniel" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["billycanyon.png"]} alt="billy_and_canyon" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["goliad.png"]} alt="goliad" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["jake.png"]} alt="jake" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["lspmissing.png"]} alt="lsp_missing_lumps" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["memow.png"]} alt="memow" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["newlemongrab.png"]} alt="lemongrab_all_better" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["princesscookie.png"]} alt="princess_cookie" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["prismo.png"]} alt="prismo" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["shelbylilbro.png"]} alt="shelby_and_little_brother" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["sweetp.png"]} alt="sweet_p" className="z-depth-4" />
-      </Col>
-
-      <Col l={3} m={4} s={6}>
-        <img src={squares["treetrunks.png"]} alt="treetrunks" className="z-depth-4" />
-      </Col>*/}
-      
 );
 
-// export default Squares;
+export default Squares;
