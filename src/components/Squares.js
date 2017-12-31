b4e7d80da9809ba03a12d5a8e01aa48362c97e46
Laura Boetcher <g33klaura@gmail.com>
2017-12-30 21:41:22 -0500

    Clean up code; add comments for onClick listener

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Col } from 'react-materialize';
 
+// Need onClick listener
+// Need to DEFINE the onClick too???************
 
-// const abracadaniel = URL('../img/abracadaniel.png');
-
-
-// Template for each repeating square?
+// Template for each repeating square
 const Squares = props => (
 
-    // Will render in row div in Game Component?
+    // Will render in row div in Game Component?  ~YES
     <Col l={3} m={4} s={6}>
       <img src={ props.image } alt={ props.name } key={ props.id } className="z-depth-4" />
     </Col>
