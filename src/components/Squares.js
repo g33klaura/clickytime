f233083451aabeb8ee7467f3a41eb05e98dcf5d8
Laura Boetcher <g33klaura@gmail.com>
2018-01-01 16:28:48 -0500

    Try adding constructor: breaks

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -7,8 +7,20 @@ import { Col } from 'react-materialize';
 
 // Template for each repeating square
 const Squares = props => {
+// class Squares extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = { 
+  //   //   friends,
+  //   //   click: false,
+  //   //   color: "testing this connects" 
+  //   // };
+  //   // this.handleClick = this.handleClick.bind(this);
+  //   console.log(this.props);
+  // }
 
   // renders with image, alt, and ids; data-click and onClick registers, just cannot target which image is clicked yet..........
+  // render() {
   return (
     <Col l={3} m={4} s={6}>
       <img src={ props.image } 
@@ -16,13 +28,14 @@ const Squares = props => {
       key={ props.id }
       id={ props.id } 
       data-click={ props.click } 
-      onClick={ props.onClick } 
+      // onClick={ props.onClick } 
       // onClick={ this.props.onClick }  //error
+      onClick={ props.onClick.bind(props.id) } 
       className="z-depth-4"
       />
     </Col>
   )
-
+// }
 };
 
 export default Squares;
