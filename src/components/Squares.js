c0bd81ff30446706d56ec5fb93ab75b80ec6ea9b
Laura Boetcher <g33klaura@gmail.com>
2018-01-02 19:29:47 -0500

    Add arrow function to onClick

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -6,39 +6,23 @@ import { Col } from 'react-materialize';
 // Need to DEFINE the onClick too************
 
 // Template for each repeating square
-const Squares = props => {
-// class Squares extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.state = { 
-  //   //   friends,
-  //   //   click: false,
-  //   //   color: "testing this connects" 
-  //   // };
-  //   // this.handleClick = this.handleClick.bind(this);
-  //   console.log(this.props);
-  // }
-
-  // renders with image, alt, and ids; data-click and onClick registers, just cannot target which image is clicked yet..........
-  // render() {
-  return (
+const Squares = props => (
     <Col l={3} m={4} s={6}>
       <img src={ props.image } 
       alt={ props.alt } 
       key={ props.id }
       id={ props.id } 
-      data-click={ props.click } 
-      // onClick={ props.onClick } 
-      // onClick={ this.props.onClick }  //error
-      onClick={ props.onClick.bind(props.id) } 
+      // Adding "() =>" tells the onClick to wait until clicked to fire the handleClick function 
+      onClick={ () => props.handleClick(props.id) } 
       className="z-depth-4"
       />
     </Col>
-  )
-// }
-};
+  );
 
 export default Squares;
 
 // onClick={ props.onClick }
 // onClick={ () => props.onClick(props.id) }
+      // onClick={ props.onClick } 
+      // onClick={ this.props.onClick }  //error
+      // onClick={ props.onClick.bind(props.id) }
