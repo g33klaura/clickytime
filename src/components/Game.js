b9439f45eb6f78a226bf414b9f7b9e04eaed694d
Laura Boetcher <g33klaura@gmail.com>
2018-01-02 19:30:03 -0500

    Add id paramater to handleClick function

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,51 +3,28 @@ import { Row } from 'react-materialize';
 import '../styles/Game.css';
 import Squares from "./Squares";
 import friends from "../friends.json";
-// import { props } from "./Squares"
 
 
 // Component class for rendering all game squares from friends.json
 class Game extends React.Component {
 
-  // Activity 16.... how does this work now?....
-  // state = {
-  //   friends,
-  //   click: false
-  // };
-
   constructor(props) {
     super(props);
     this.state = { 
     // state = {
-      friends,
-      // click: false,
-      color: "testing this connects" 
+      friends, 
     };
-    this.handleClick = this.handleClick.bind(this);
-    console.log(this.props);
+    // console.log(this.props);
   }
 
-  handleClick = () => {
-
-    //   this.setState({
-    //     click: true
-    //   });
-    //   console.log("Image clicked");
-
-    // this.setState( () => ({
-    //   friends: {
-    //     click: true
-    //   }
-    // }) )
+  handleClick = (id) => {
 
     // this.setState({
     //   friends
     // });
     console.log("Image clicked");
-    console.log(this);
+    console.log(id);
 
-    // console.log(object);
-    // ^^start over trying to determine which image was clicked....
   }
 
   // Use "building block" component from Squares to render each image in the materialize divs
@@ -62,10 +39,7 @@ class Game extends React.Component {
               alt={ friend.name }
               key={ friend.id }
               id={ friend.id }
-              // click={ this.state.click }
-              click={ friend.click }
-              onClick={ this.handleClick }
-              color-test={ this.state.color }
+              handleClick={ this.handleClick }
             />
           ))}
         </Row>
@@ -76,10 +50,3 @@ class Game extends React.Component {
 }
 
 export default Game;
-
-// This was between <Row> </Row>
-// <Squares 
-//             image={ friends[0].image }
-//             alt={ friends[0].name }
-//             key={ friends[0].id }
-// />
