29b109845b791f699ac4db0e7b001ea7909894b6
Laura Boetcher <g33klaura@gmail.com>
2018-01-05 23:50:05 -0500

    Clean up comments; remove Flip-Move

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Col } from 'react-materialize';
-// import FlipMove from 'react-flip-move';
 
 
 // Template for each repeating image square
 const Squares = props => (
-  // <FlipMove duration={750} easing="ease-out">
     <Col l={3} m={4} s={6}>
       <img src={ props.image } 
       alt={ props.alt } 
@@ -14,11 +12,9 @@ const Squares = props => (
       // click={ props.click.toString() }
       // Adding "() =>" tells the onClick to wait until clicked to fire the handleClick function 
       onClick={ () => props.handleClick(props.id) }
-      // onClick={ props.handleClick }
       className="z-depth-4"
       />
     </Col>
-    // </FlipMove>
   );
 
 export default Squares;
