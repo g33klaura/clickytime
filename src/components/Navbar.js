e263668c1a6c269d99b84df22fae0ded537cc591
Laura Boetcher <g33klaura@gmail.com>
2018-01-07 16:29:39 -0500

    Edit wording & add comment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,12 @@
-// props component here
-//  import in Game.js
-// then able to update score & message on here?
-// render <Navbar /> on App.js still? ~YES
-
 import React from 'react';
 import '../styles/Navbar.css';
 
 
+// Template to hold scoring and player message
 const Navbar = props => (
   <header className="App-header">
     <h1 className="App-title">ClickyTime</h1>
-    {/*<p className="score">Score goes here!</p>*/}
-    <p className="score">Score: { props.score } out of 12 | Top Score: { props.topScore }</p>
+    <p className="score">Score: { props.score } of 12 | Top Score: { props.topScore }</p>
     <p className="message">{ props.message }</p>
   </header>
 );
